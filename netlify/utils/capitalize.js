04bfdb36af7e70224be2a325d0c2adbc8fa7c5e2
Rithvik <x394@pm.me>
2023-03-29 05:24:49 +0530

    Switched back to ESM as 'fetch' api not defined

--- netlify/utils/capitalize.js
+++ netlify/utils/capitalize.js
@@ -1,8 +1,8 @@
-module.exports = function capitalize(str) {
+export default function capitalize(str) {
     return str
         .match(/[^\s]+/g)
         .map(function (word) {
             return word[0].toUpperCase() + word.substr(1);
         })
         .join(" ");
-};
+}
