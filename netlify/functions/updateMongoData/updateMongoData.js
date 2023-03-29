aafc45b6220194d192c79d48676205a8c9ce923e
Rithvik <x394@pm.me>
2023-03-29 08:47:05 +0530

    debugging

--- netlify/functions/updateMongoData/updateMongoData.js
+++ netlify/functions/updateMongoData/updateMongoData.js
@@ -1,7 +1,6 @@
 import { schedule } from "@netlify/functions";
 import { MongoClient } from "mongodb";
 import remapZoteroData from "../../utils/remapZoteroData";
-import fetch from "node-fetch";
 
 const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
