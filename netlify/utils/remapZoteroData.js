0fe2c71fc17b96765ec1140061021efe5801b54e
Rithvik <x394@pm.me>
2023-03-29 21:37:23 +0530

    refactored database schema

--- netlify/utils/remapZoteroData.js
+++ netlify/utils/remapZoteroData.js
@@ -19,8 +19,33 @@ function checkFav(tags) {
     return remapTags(tags).includes("Favourite");
 }
 
-const remapZoteroData = (inputData) => {
-    const remappedData = inputData.map(({ data }) => ({
+const remapZoteroData = (patchedItems) => {
+    const remappedData = inputData.map((item, index, inputData) => ({
+        abstractNote: item.abstractNote,
+        alt: getItemAlt()
+        "alt": "",
+        "authors": [],
+        "dateAdded": "2021-11-30T11:08:58.000Z",
+        "dateModified": "2021-11-30T11:08:58.000Z",
+        "edition": null,
+        "favourite": true,
+        "image": "",
+        "institution": null,
+        "itemType": "Webpage",
+        "key": "E3CDZKUV",
+        "pages": null,
+        "publicationTitle": null,
+        "publisher": null,
+        "series": null,
+        "tags": [
+            "Favourite"
+        ],
+        "title": "Sturdy cattle that sustain fragile communities",
+        "url": "https://ruralindiaonline.org/en/articles/sturdy-cattle-that-sustain-fragile-communities/",
+        "version": 98,
+        "volume": null,
+        "websiteTitle": "People's Archive of Rural India",
+        "year": 2019
         key: data.key,
         version: data.version,
         parentItem: data.parentItem,
