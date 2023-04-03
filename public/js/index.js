ad913a02e1b5179e595d489718af6ea9e501a301
Rithvik <x394@pm.me>
2023-04-04 01:43:37 +0530

    Added loading icon and scroll to top button

--- public/js/index.js
+++ public/js/index.js
@@ -3,40 +3,43 @@ Sticky Header
 ===========================*/
 
 const MyStickyHeader = true,
-    header = document.querySelector("header"),
-    doc = document.querySelector(":root"),
-    headerHeight = header.offsetHeight;
+	header = document.querySelector("header"),
+	doc = document.querySelector(":root"),
+	scrollToTopBtn = document.querySelector(".scrollToTop"),
+	headerHeight = header.offsetHeight;
 
 let lastScrollTop = 0;
 
 doc.style.setProperty("--headerH", headerHeight + "px");
 
 if (MyStickyHeader) {
-    header.style.setProperty("position", "fixed");
+	header.style.setProperty("position", "fixed");
 }
 
 if (MyStickyHeader) {
-    window.addEventListener("scroll", function () {
-        scrollTop = window.pageYOffset;
+	window.addEventListener("scroll", function () {
+		scrollTop = window.pageYOffset;
 
-        if (scrollTop > 0) {
-            header.classList.add("scroll");
-        } else {
-            header.classList.remove("scroll");
-        }
+		if (scrollTop > 0) {
+			header.classList.add("scroll");
+		} else {
+			header.classList.remove("scroll");
+		}
 
-        if (scrollTop > 300) {
-            header.classList.add("check");
-        } else {
-            header.classList.remove("check");
-        }
+		if (scrollTop > 300) {
+			header.classList.add("check");
+			scrollToTopBtn.classList.add("show");
+		} else {
+			header.classList.remove("check");
+			scrollToTopBtn.classList.remove("show");
+		}
 
-        if (scrollTop > 300 && scrollTop > lastScrollTop) {
-            header.classList.add("exit");
-        } else {
-            header.classList.remove("exit");
-        }
+		if (scrollTop > 300 && scrollTop > lastScrollTop) {
+			header.classList.add("exit");
+		} else {
+			header.classList.remove("exit");
+		}
 
-        lastScrollTop = scrollTop;
-    });
+		lastScrollTop = scrollTop;
+	});
 }
