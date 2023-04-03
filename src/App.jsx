acb51046df2228ef9e9f6950591d1080105a75a1
Rithvik <x394@pm.me>
2023-04-03 21:03:38 +0530

    Design overhaul

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Homepage from './components/Homepage'
-import useScript from './hooks/useScript'
-import './index.css'
+import React from "react";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import useScript from "./hooks/useScript";
+import "./assets/css/index.css";
 
 function App() {
-  useScript('/js/index.js')
-  return (
-    <>
-    <Header />
-    <main id="content">
-      <Homepage />
-    </main>
-    <Footer />
-    </>
-  )
+	useScript("/js/index.js");
+	return (
+		<>
+			<Header />
+			<main id="content">
+				<Homepage />
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
